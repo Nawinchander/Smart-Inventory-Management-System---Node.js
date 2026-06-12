346c0c84f02b7769e719f3504dc5723f9dfb3055
Nawin chander <nawinnapzy289@gmail.com>
2026-06-13 01:58:53 +0530

    add

---
+++ src/config/db.mongo.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const connectMongo = async ()=>{
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("Mongo connected");
+}
+
+module.exports = connectMongo;
+
+
