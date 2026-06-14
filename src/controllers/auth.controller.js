c26a8bad0a719956341d5712cca4938fee0d804f
Nawin chander <nawinnapzy289@gmail.com>
2026-06-14 10:47:07 +0530

    add

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+exports.login = async(req,res)=>{
+
+    const token = jwt.sign(
+        {
+            id:1
+        },
+        process.env.JWT_SECRET
+    );
+
+    res.json({
+        token
+    });
+}
