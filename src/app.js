447aa7a989ba19009e8ba63359387438cb7d426f
Nawin chander <nawinnapzy289@gmail.com>
2026-06-17 15:49:05 +0530

    add

--- src/app.js
+++ src/app.js
@@ -9,5 +9,18 @@ app.use(express.json());
 
 app.use("/api/auth",authRoutes);
 app.use("/api/products",productRoutes);
+const client = require("./config/prometheus");
+
+app.get("/metrics", async (req, res) => {
+
+    res.set(
+        "Content-Type",
+        client.register.contentType
+    );
+
+    res.end(
+        await client.register.metrics()
+    );
+});
 
 module.exports = app;
