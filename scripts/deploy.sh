9b0250dadcf6112a6a7c4232e2b7fba0700815f3
Nawin chander <nawinnapzy289@gmail.com>
2026-06-19 15:33:01 +0530

    add

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,3 +15,14 @@ docker push username/inventory-app
 echo "Deploying to Kubernetes..."
 
 kubectl apply -f k8s/
+
+
+
+// chmod +x scripts/deploy.sh
+./scripts/deploy.sh
+
+#!/bin/bash
+
+kubectl rollout undo deployment/inventory-deployment
+./scripts/rollback.sh
+
