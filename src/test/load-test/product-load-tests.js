7784b96ced7de1647ab75d3311be040ecc210092
Nawin chander <nawinnapzy289@gmail.com>
2026-06-22 11:44:27 +0530

    add

---
+++ src/test/load-test/product-load-tests.js
@@ -0,0 +1,20 @@
+import http from "k6/http";
+
+import { sleep } from "k6";
+
+export let options = {
+
+    vus: 50,
+    duration: "30s"
+
+};
+
+export default function () {
+
+    http.get(
+        "http://localhost:5000/api/products"
+    );
+
+    sleep(1);
+
+}
